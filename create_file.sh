a640a94e068ba5cac95ad21ae2932179372f898a
mtmar <moto.perin@gmail.com>
2021-07-17 14:32:20 +0900

    ファイル、ディレクトリの名前変更

--- create_file.sh
+++ create_file.sh
@@ -8,36 +8,34 @@ cat <<ENDOFTEXT
 Usage: $0 [< introduction >]
 
 Introduction
-  This script creates directory and files, Given execution 
+  This script creates files, Given execution 
   rights in the generated files.
 
-  Argument 1 becomes the <directory> name.
-
-  Create as many files as given in argument 2.
+  Create as many files as given in argument 1.
 
   If the extension is .rb, write #!/usr/bin/env ruby.
   If the extension is .sh, write #!/bin/bash.
 
 Usage
-  1. Write <dir name> <number of files>
-  2. for example → ./useful.sh "src" "3" 
+  1. Write <number of files>
+  2. for example → ./useful.sh "3" 
   3. Type in the file name you want to create on the command line
 -----------------------------------------------------------------
 ENDOFTEXT
 }
 
-if [ "$#" != "2" ]; then
+if [ "$#" != "1" ]; then
   usage
   exit
 fi 
 # usage = showing usage when there are no two arguments
 # exit = stop execution because of error
 
-mkdir ./$1 
-for number in `seq $2`; do
+
+for number in `seq $1`; do
   read -p "input file name >> " file
   # Type in the file name you want to create
-  touch ./$1/$file && chmod 755 ./$1/$file
-  [ ${file##*.} = "sh" ] && echo "#!/bin/bash" > ./$1/$file # The extension is judgement
-  [ ${file##*.} = "rb" ] && echo "#!/usr/bin/env ruby" > ./$1/$file
+  touch ./$file && chmod 755 ./$file
+  [ ${file##*.} = "sh" ] && echo "#!/bin/bash" > ./$file # The extension is judgement
+  [ ${file##*.} = "rb" ] && echo "#!/usr/bin/env ruby" > ./$file
 done
