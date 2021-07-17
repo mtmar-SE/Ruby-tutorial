385611d15f145169a019bcb013db9d912f92deec
mtmar <moto.perin@gmail.com>
2021-07-17 15:24:45 +0900

    多重代入を行なっているスクリプトになります。

---
+++ Ruby/lib/multiple_assignment.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+#!/usr/bin/env ruby
+
+# It is better not to use too much
+
+# Formula for finding 14 quotients and remainders
+quotient, remainder = 14.divmod(3)
+puts "商=#{quotient}, 余り=#{remainder}"
