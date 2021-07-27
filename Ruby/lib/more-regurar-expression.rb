d551e683bb3ccc7f52f0cad54f8075431680911d
mtmar <moto.perin@gmail.com>
2021-07-27 18:56:19 +0900

    正規表現

--- Ruby/lib/more-regurar-expression.rb
+++ Ruby/lib/more-regurar-expression.rb
@@ -6,3 +6,10 @@ type=xml; filename=posts.xml; size=2048;
 TEXT
 puts text.scan(/(?<=filename=)[^;]+/)
 # ファイル名を抜き出す
+
+txt = <<-TEXT
+John:guitar, George:guitar, Paul:bass, Ringo:drum
+Freddie:vocal, Brian:guitar, John:bass, Roger:drum
+TEXT
+puts txt.scan(/\w+(?=:bass)/)
+# bassの直前の一文字以上の文字列を抜き出す
